33977a5c0c8f3cc8808cd1254c4aee95f5a8dc93
rajputkarsh <r.utkarsh.0010@gmail.com>
2023-04-09 02:04:39 +0530

    Update advertisement

--- client/src/components/widgets/advertisement/index.tsx
+++ client/src/components/widgets/advertisement/index.tsx
@@ -1,4 +1,4 @@
-import { Typography, useTheme } from "@mui/material";
+import { Button, Typography, useTheme } from "@mui/material";
 import FlexContainer from "../../../containers/flexContainer";
 import WidgetContainer from "../../../containers/widgetContainer";
 import { CustomTheme } from "../../../interfaces";
@@ -21,16 +21,26 @@ const Advertisement = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/info4.jpeg"
+        src="https://firebasestorage.googleapis.com/v0/b/social-media-70beb.appspot.com/o/Screenshot%20from%202023-04-09%2001-52-12.png?alt=media&token=26b4102a-73c4-44b6-8526-6ae11df72680"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexContainer>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>Best Developer</Typography>
+        <Button
+          size="small"
+          type="submit"
+          onClick={() => window.open('https://utkarshrajput.com', '_blank')}
+          sx={{
+            backgroundColor: palette.primary.main,
+            color: palette.background.alt,
+            "&:hover": { color: palette.primary.main },
+          }}        
+        >
+          Click Here
+        </Button>
       </FlexContainer>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+          Hire the best MERN Stack developer today! Contact now for more details. <br/> Offer valid till stock lasts!
       </Typography>
     </WidgetContainer>
   );
